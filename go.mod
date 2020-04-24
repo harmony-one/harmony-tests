bd2f44bfb9e034e779fb6a058e7cfb155bbc730d
Sebastian Johnsson <sebastian.johnsson@gmail.com>
2020-04-24 19:58:25 +0700

    Update installer scripts

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/harmony-one/harmony-tests
 
 go 1.14
 
-require github.com/harmony-one/harmony-tf v0.0.0-20200422033610-451d7f296f1e
+require github.com/harmony-one/harmony-tf v0.0.0-20200424125729-8579dc6ab1f2
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "Installing Harmony Tx Tests"
+echo "Installing Harmony Tests"
 rm -rf hmy && curl -LO https://harmony.one/hmycli && mv hmycli hmy && chmod u+x hmy
 rm -rf tests && curl -LOs http://tools.harmony.one.s3.amazonaws.com/release/linux-x86_64/harmony-tests/tests && chmod u+x tests
 rm -rf testcases && curl -LOs http://tools.harmony.one.s3.amazonaws.com/release/linux-x86_64/harmony-tests/testcases.tar.gz && tar -xzf testcases.tar.gz && rm -rf testcases.tar.gz
